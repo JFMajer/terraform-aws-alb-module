067c9b953d3dd4c42552f9902b8de4f16e0531e0
JFMajer <jakubmajer@live.com>
2023-03-05 12:25:13 +0100

    first commit

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+output "load_balancer_dns" {
+  value = aws_lb.asg_lb.dns_name
+  description = "DNS name of the load balancer"
+}
+
+output http_url {
+  value = "http://${aws_lb.asg_lb.dns_name}"
+}
+
+output https_url {
+  value = "https://${aws_lb.asg_lb.dns_name}"
+}
+
+output "alb_zone_id" {
+  value = aws_lb.asg_lb.zone_id
+  description = "Zone ID of the load balancer"
+}
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "cluster_name" {
+    description = "Name of the cluster"
+    type        = string
+}
+
+variable "custom_tags" {
+    description = "Custom tags to be added to resources"
+    type = map(string)
+    default = {}
+}
+
+variable "alb_subnets" {
+    description = "The subnets to deploy the ALB into"
+    type = list(string)
+}
+
+variable "vpc_id" {
+    description = "The VPC to deploy the resources into"
+    type = string
+}
+
+variable "certificate_arn" {
+    description = "The ARN of the certificate to use for HTTPS"
+    type = string
+}
