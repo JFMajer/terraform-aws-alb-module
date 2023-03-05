1b2c1a58771f47173d94834dd17f1378e1837b79
JFMajer <jakubmajer@live.com>
2023-03-05 12:31:56 +0100

    moved ALB to separate module

--- outputs.tf
+++ outputs.tf
@@ -15,3 +15,13 @@ output "alb_zone_id" {
   value = aws_lb.asg_lb.zone_id
   description = "Zone ID of the load balancer"
 }
+
+output "alb_security_group_id" {
+  value = aws_security_group.asg_lb_sg.id
+  description = "Security group ID of the load balancer"
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.asg_tg.arn
+  description = "Target group ARN of the load balancer"
+}
